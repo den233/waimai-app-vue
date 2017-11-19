748d83f8cafdf7b686be6bb454d29327b6d041ae
wangzhaolun <leibo_369@163.com>
2017-11-19 23:21:23 +0800

    timestamp convert to date format

--- src/common/js/formatDate.js
+++ src/common/js/formatDate.js
@@ -1,9 +1,7 @@
 export function formatDate (date, fmt) {
-  console.log(date, fmt);
   if (/(y+)/.test(fmt)) {
     fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
   }
-  console.log(fmt);
   let o = {
     'M+': date.getMonth() + 1,
     'd+': date.getDate(),
@@ -13,12 +11,10 @@ export function formatDate (date, fmt) {
   };
   for (let k in o) {
     if (new RegExp(`(${k})`).test(fmt)) {
-      console.log(RegExp);
       let str = o[k] + '';
       fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str));
     }
   }
-  console.log(fmt);
   return fmt;
 };
 function padLeftZero (str) {
