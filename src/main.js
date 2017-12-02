022ab9a59fa7ea0823fc8fe64d7b57e57229ba95
wangzhaolun <leibo_369@163.com>
2017-12-02 16:53:14 +0800

    modify http request data type

--- src/main.js
+++ src/main.js
@@ -9,11 +9,17 @@ import Ratings from './components/ratings/ratings';
 import Seller from './components/seller/seller';
 import './common/stylus/index.stylus';
 
-// Vue.config.productionTip = false
+import store from './store/index';
+Vue.config.productionTip = false;
+
 Vue.use(VueRouter);
 Vue.use(VueResource);
+// 如果Web服务器无法处理编码为application/json的请求，你可以启用emulateJSON选项。启用该选项后，请求会以application/x-www-form-urlencoded作为MIME type。
+Vue.http.options.emulateJSON = true;
+// 如果Web服务器无法处理PUT, PATCH和DELETE这种REST风格的请求，你可以启用enulateHTTP现象。启用该选项后，请求会以普通的POST方法发出，并且HTTP头信息的X-HTTP-Method-Override属性会设置为实际的HTTP方法。
+Vue.http.options.emulateHTTP = true;
+
 let router = new VueRouter({
-  mode: 'history',
   routes: [
     {
       path: '/',
@@ -37,6 +43,7 @@ let router = new VueRouter({
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: {App}
 });
