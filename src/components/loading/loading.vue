a369fafd2cb9acf02673abd7ad98f2cd6301a591
wangzhaolun <leibo_369@163.com>
2017-12-02 17:05:14 +0800

    loading component

---
+++ src/components/loading/loading.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="loading"></div>
+</template>
+
+<script type="es6">
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .loading
+    position fixed
+    top 0
+    left 0
+    width 100%
+    height 100%
+    z-index 10000
+    background-image url(./loading.gif)
+    background-repeat no-repeat
+    background-position center center
+    background-color rgba(0,0,0,0)
+</style>
